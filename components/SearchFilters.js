0adbe02cfb1e9289018a5bdc393238ec0b916055
Stephen Magrowski <stephen.magrowski@gmail.com>
2023-04-03 21:42:46 -0400

    Update SearchFilters.js

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,23 +1,43 @@
-/********************************************************************/
-/*  Author:     Stephen Magrowski                                   */
-/*  Created:    June 9, 2021                                        */
-/*  Filename:   SearchFilters.js                                    */
-/*  Purpose:    Used in MapScreen and DiscoverScreen. Reusable      */
-/*              component for search filter buttons.                */
-/*                                                                  */
-/********************************************************************/
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Dimensions, Animated, TouchableWithoutFeedback } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+const screenWidth = Dimensions.get('window').width;
 
 const FiltersComponent = (props) => {
+  // Animated value for fade in effect, used on bottom bar of the selected button
+  const fadeAnim = props.firstMount ? 1 : new Animated.Value(0);
+  // State for handling the touchable opacity effect for filter buttons
+  const [isPressed, setIsPressed] = useState(1);
+  // Returns true or false depending on if the button is selected or not
+  const isSelected = props.textColor == 'red' ? true : false
+
+  useEffect(() => {
+    props.firstMount ? null :
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 400,
+      useNativeDriver: true
+    }).start();
+  }, [props.selectedId]);
+
   return (
     <View style={styles.searchButton}>
-      <TouchableOpacity onPress={props.onPress}>
-        <View style={styles.innerContainer}>
-          <Text style={[ styles.buttonText, { color: props.textColor } ]}>{props.category}</Text>
+      <TouchableWithoutFeedback
+          onPress={props.onPress}
+          onPressIn={() => setIsPressed(0.6)}
+          onPressOut={() => setIsPressed(1)}>
+        <View style={[ styles.innerContainer, { bottom: isSelected ? 0 : screenWidth < 670 ? -2.4 : -3 } ]}>
+          {( props.category == 'Popular' ) ?
+            <Ionicons name={'ios-star'}
+              size={(screenWidth < 670 ? 17.5 : 25)}
+              color={props.textColor}
+              style={[styles.starIcon, { opacity: isPressed, top: -1 } ]}/>
+          : null }
+          <Text style={[ styles.buttonText, { color: props.textColor, opacity: isPressed } ]}>{props.category}</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableWithoutFeedback>
+      { isSelected ? <Animated.View style={[styles.selectedView, { opacity: fadeAnim } ]}/> : null }
     </View>
   );
 }
@@ -28,6 +48,12 @@ const styles = StyleSheet.create({
   searchButton: {
     flex: 1,
     backgroundColor: 'transparent'
+  },
+  selectedView: {
+    width: '100%',
+    height: (screenWidth < 670 ? 2.5 : 3),
+    backgroundColor: 'red',
+    borderRadius: (screenWidth < 670 ? 1 : 4),
   },
   buttonText: {
     fontWeight: 'bold',
@@ -40,5 +66,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: 'transparent',
+  },
+  starIcon: {
+    backgroundColor: 'transparent',
+    marginRight: (screenWidth < 670 ? 5.5 : 8),
   },
 });
