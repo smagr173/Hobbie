1df828700773db4e2b91d384b90c7d0e67d62972
Stephen Magrowski <stephen.magrowski@gmail.com>
2023-04-06 02:56:52 -0400

    Delete system_admins_helper.rb

--- rails_api/helpers/system_admins_helper.rb
+++
@@ -1,9 +0,0 @@
-module SystemAdminsHelper
-
-  def create_system_admin_link
-    link_to new_system_admin_path, data: { "title" => "New User" }, title: "Create User" do 
-      icon("plus", "Add Root User")
-    end
-  end
-	
-end
